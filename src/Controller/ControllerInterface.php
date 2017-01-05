c59fae40c265ae861d4e7fa6e9fe0ad1bf632f2e
ise <iam@ise.me.uk>
2017-01-05 02:28:27 +0000

    doc update

--- src/Controller/ControllerInterface.php
+++ src/Controller/ControllerInterface.php
@@ -2,6 +2,7 @@
 
 namespace Ise\Bread\Controller;
 
+use Zend\Stdlib\ResponseInterface;
 use Zend\View\Model\ViewModel;
 
 interface ControllerInterface
@@ -10,49 +11,49 @@ interface ControllerInterface
     /**
      * Browse action
      *
-     * @return ViewModel
+     * @return ViewModel|ResponseInterface
      */
     public function browseAction();
 
     /**
      * Read action
      *
-     * @return ViewModel
+     * @return ViewModel|ResponseInterface
      */
     public function readAction();
 
     /**
      * Add action
      *
-     * @return ViewModel
+     * @return ViewModel|ResponseInterface
      */
     public function addAction();
 
     /**
      * Edit action
      *
-     * @return ViewModel
+     * @return ViewModel|ResponseInterface
      */
     public function editAction();
 
     /**
      * Delete action
      *
-     * @return ViewModel
+     * @return ViewModel|ResponseInterface
      */
     public function deleteAction();
 
     /**
      * Disable action
      *
-     * @return ViewModel
+     * @return ViewModel|ResponseInterface
      */
     public function disableAction();
 
     /**
      * Enable action
      *
-     * @return ViewModel
+     * @return ViewModel|ResponseInterface
      */
     public function enableAction();
 }

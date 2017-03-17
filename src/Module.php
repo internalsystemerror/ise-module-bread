8ddbd80460eefd2655b94f2f4e1ebe6d6ddc39ce
ise <iam@ise.me.uk>
2017-03-17 12:37:47 +0000

    Remove from module dependencies

--- src/Module.php
+++ src/Module.php
@@ -3,27 +3,22 @@
 namespace Ise\Bread;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
 use Zend\ModuleManager\Feature\InitProviderInterface;
-use Zend\ModuleManager\ModuleManagerInterface ;
+use Zend\ModuleManager\ModuleManagerInterface;
 
-class Module implements
-    ConfigProviderInterface,
-    DependencyIndicatorInterface,
-    InitProviderInterface
+class Module implements ConfigProviderInterface, InitProviderInterface
 {
 
     /**
      * {@inheritDoc}
      */
-    public function init(ModuleManagerInterface  $moduleManager)
+    public function init(ModuleManagerInterface $moduleManager)
     {
         $eventManager = $moduleManager->getEventManager();
-        
+
         $configListener = new Listener\ConfigListener();
         $configListener->attach($eventManager);
     }
-
 
     /**
      * {@inheritDoc}
@@ -31,16 +26,5 @@ class Module implements
     public function getConfig()
     {
         return include realpath(__DIR__ . '/../config/module.config.php');
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getModuleDependencies()
-    {
-        return [
-            'DoctrineModule',
-            'DoctrineORMModule',
-        ];
     }
 }
